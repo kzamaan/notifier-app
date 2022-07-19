3410a2ea18f2647cbf9674a8666b40e18183d588
Kamruzzaman <zamandotbd@gmail.com>
2022-07-19 18:18:48 +0600

    added sync button on home screen

--- app/src/main/java/com/softxilla/notification_forwarder/ui/MainActivity.kt
+++ app/src/main/java/com/softxilla/notification_forwarder/ui/MainActivity.kt
@@ -8,16 +8,29 @@ import android.os.Bundle
 import android.provider.Settings
 import android.provider.Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS
 import android.text.TextUtils
+import android.util.Log
 import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
+import com.android.volley.AuthFailureError
+import com.android.volley.Response
+import com.android.volley.VolleyLog
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import dagger.hilt.android.AndroidEntryPoint
 import com.softxilla.notification_forwarder.R
 import com.softxilla.notification_forwarder.base.BaseActivity
+import com.softxilla.notification_forwarder.database.MessageDatabaseHelper
+import com.softxilla.notification_forwarder.network.NetworkHelper
+import com.softxilla.notification_forwarder.utils.LoadingUtils
 import com.softxilla.notification_forwarder.utils.visible
+import org.json.JSONArray
+import org.json.JSONObject
 
 
 @AndroidEntryPoint
@@ -25,11 +38,13 @@ class MainActivity : BaseActivity() {
     private lateinit var rlToolbar: Toolbar
     private lateinit var tvTitle: TextView
     private lateinit var ivBackButton: ImageView
+    private lateinit var messageDatabaseHelper: MessageDatabaseHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        messageDatabaseHelper = MessageDatabaseHelper(this)
+        loadingUtils = LoadingUtils(this)
         rlToolbar = findViewById(R.id.toolbar_root)
         tvTitle = findViewById(R.id.tv_toolbar_title)
         ivBackButton = findViewById(R.id.iv_back_button)
@@ -80,6 +95,15 @@ class MainActivity : BaseActivity() {
         return true
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.syncMessage -> {
+                syncOfflineMessage()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     private fun isNotificationServiceEnabled(): Boolean {
         val pkgName = packageName
         val flat = Settings.Secure.getString(
@@ -98,5 +122,69 @@ class MainActivity : BaseActivity() {
             }
         }
         return false
+    }
+
+    private fun syncOfflineMessage() {
+        val helper = NetworkHelper(applicationContext)
+        if (helper.isNetworkConnected()) {
+            Log.d("status", "Online, Internet Available")
+            val messages = messageDatabaseHelper.getUnSyncedMessage()
+            val jsonArray = JSONArray()
+            if (messages.moveToFirst()) {
+                do {
+                    val messageObject = JSONObject()
+                    val rowId = messages.getColumnIndex(MessageDatabaseHelper.ID)
+                    val locAppName = messages.getColumnIndex(MessageDatabaseHelper.APP_NAME)
+                    val locPackage = messages.getColumnIndex(MessageDatabaseHelper.PACKAGE_NAME)
+                    val locTitle = messages.getColumnIndex(MessageDatabaseHelper.ANDROID_TITLE)
+                    val locText = messages.getColumnIndex(MessageDatabaseHelper.ANDROID_TEXT)
+                    val createdAt = messages.getColumnIndex(MessageDatabaseHelper.CREATED_AT)
+                    messageObject.put("app_name", messages.getString(locAppName))
+                    messageObject.put("package_name", messages.getString(locPackage))
+                    messageObject.put("android_title", messages.getString(locTitle))
+                    messageObject.put("android_text", messages.getString(locText))
+                    messageObject.put("created_at", messages.getString(createdAt))
+                    jsonArray.put(messageObject)
+                    messageDatabaseHelper.updateMessageStatus(messages.getInt(rowId))
+                } while (messages.moveToNext())
+
+                val offlineMessageObject: MutableMap<String, String> = HashMap()
+                offlineMessageObject["messages"] = jsonArray.toString()
+                Log.d("offlineMessage", offlineMessageObject.toString())
+                syncOfflineMessageSubmit(offlineMessageObject)
+            }
+        } else {
+            Toast.makeText(this, "No Internet Connection...", Toast.LENGTH_SHORT).show()
+            Log.d("status", "Offline, No Internet")
+        }
+    }
+
+    private fun syncOfflineMessageSubmit(postObject: Map<String, String>) {
+        val url = "https://softxilla.com/api/sync-offline-message"
+        val requestQueue = Volley.newRequestQueue(this)
+        Log.d("d", "Data: " + android.R.attr.data.toString())
+        Log.d("d", "postObject: $postObject")
+        loadingUtils.isLoading(true)
+        val jsonObjRequest: StringRequest =
+            object : StringRequest(Method.POST, url,
+                Response.Listener {
+                    Log.d("sync_offline", it.toString())
+                    loadingUtils.isLoading(false)
+                }, Response.ErrorListener { error ->
+                    VolleyLog.d("volley", "Error: " + error.message)
+                    loadingUtils.isLoading(false)
+                    error.printStackTrace()
+                }) {
+                override fun getBodyContentType(): String {
+                    return "application/x-www-form-urlencoded; charset=UTF-8"
+                }
+
+                @Throws(AuthFailureError::class)
+                override fun getParams(): Map<String, String> {
+                    loadingUtils.isLoading(false)
+                    return postObject
+                }
+            }
+        requestQueue.add(jsonObjRequest)
     }
 }
