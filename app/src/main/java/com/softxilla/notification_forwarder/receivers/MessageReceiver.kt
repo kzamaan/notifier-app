74f32ed9245f6359d7a470f18af3d3d091fe4b00
Kamruzzaman <zamandotbd@gmail.com>
2022-07-25 10:53:13 +0600

    separate api from BroadcastReceiver

--- app/src/main/java/com/softxilla/notification_forwarder/receivers/MessageReceiver.kt
+++ app/src/main/java/com/softxilla/notification_forwarder/receivers/MessageReceiver.kt
@@ -6,16 +6,10 @@ import android.content.Intent
 import android.telephony.SmsMessage
 import android.provider.Telephony.Sms.Intents.getMessagesFromIntent
 import android.util.Log
-import com.android.volley.AuthFailureError
-import com.android.volley.Response
-import com.android.volley.VolleyLog
-import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
-import com.google.gson.Gson
-import com.softxilla.notification_forwarder.data.response.OnlineResponse
 import com.softxilla.notification_forwarder.database.MessageDatabaseHelper
 import com.softxilla.notification_forwarder.database.SharedPreferenceManager
 import com.softxilla.notification_forwarder.network.NetworkHelper
+import com.softxilla.notification_forwarder.utils.sendNotificationPost
 import dagger.hilt.android.AndroidEntryPoint
 import java.util.Locale
 import javax.inject.Inject
@@ -56,7 +50,7 @@ class MessageReceiver : BroadcastReceiver() {
                     val lastMessage = databaseHelper.saveMsgSQLite(originatingAddress, messageBody)
                     postObject["app_id"] = lastMessage.toString()
                     if (helper.isNetworkConnected()) {
-                        sendNotificationPost(postObject, mContext)
+                        sendNotificationPost(mContext, postObject)
                     }
                     Log.d("Match Title", "Match Title: $originatingAddress")
                 } else {
@@ -69,36 +63,5 @@ class MessageReceiver : BroadcastReceiver() {
     private fun matchMessageTitle(title: String): Boolean {
         val stringUpper = title.uppercase(Locale.ROOT).trim()
         return stringUpper == BKASH || stringUpper == NAGAD || stringUpper == UPAY || stringUpper == ROCKET
-    }
-
-    private fun sendNotificationPost(postObject: Map<String, String>, mContext: Context) {
-        val url = "https://softxilla.com/api/store-notification"
-        val requestQueue = Volley.newRequestQueue(mContext)
-        val jsonObjRequest: StringRequest =
-            object : StringRequest(Method.POST, url,
-                Response.Listener {
-                    Log.d("Response", it.toString())
-                    val response = Gson().fromJson(it.toString(), OnlineResponse::class.java)
-                    if (response.status) {
-                        Log.d("Response", "Success")
-                        val databaseHelper = MessageDatabaseHelper(mContext)
-                        databaseHelper.updateMessageStatus(response.appId)
-                    } else {
-                        Log.d("Response", "Failed")
-                    }
-                }, Response.ErrorListener { error ->
-                    VolleyLog.d("volley", "Error: " + error.message)
-                    error.printStackTrace()
-                }) {
-                override fun getBodyContentType(): String {
-                    return "application/x-www-form-urlencoded; charset=UTF-8"
-                }
-
-                @Throws(AuthFailureError::class)
-                override fun getParams(): Map<String, String> {
-                    return postObject
-                }
-            }
-        requestQueue.add(jsonObjRequest)
     }
 }
--- app/src/main/java/com/softxilla/notification_forwarder/utils/ExtraUtils.kt
+++ app/src/main/java/com/softxilla/notification_forwarder/utils/ExtraUtils.kt
@@ -10,10 +10,45 @@ import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.google.gson.Gson
 import com.softxilla.notification_forwarder.data.response.OfflineResponse
+import com.softxilla.notification_forwarder.data.response.OnlineResponse
 import com.softxilla.notification_forwarder.database.MessageDatabaseHelper
 import org.json.JSONArray
 import org.json.JSONObject
 
+fun sendNotificationPost(
+    mContext: Context,
+    postObject: Map<String, String>
+): Boolean {
+    val url = "https://softxilla.com/api/store-notification"
+    val requestQueue = Volley.newRequestQueue(mContext)
+    val jsonObjRequest: StringRequest =
+        object : StringRequest(Method.POST, url,
+            Response.Listener {
+                Log.d("Response", it.toString())
+                val response = Gson().fromJson(it.toString(), OnlineResponse::class.java)
+                if (response.status) {
+                    Log.d("Response", "Success")
+                    val databaseHelper = MessageDatabaseHelper(mContext)
+                    databaseHelper.updateMessageStatus(response.appId)
+                } else {
+                    Log.d("Response", "Failed")
+                }
+            }, Response.ErrorListener { error ->
+                VolleyLog.d("volley", "Error: " + error.message)
+                error.printStackTrace()
+            }) {
+            override fun getBodyContentType(): String {
+                return "application/x-www-form-urlencoded; charset=UTF-8"
+            }
+
+            @Throws(AuthFailureError::class)
+            override fun getParams(): Map<String, String> {
+                return postObject
+            }
+        }
+    requestQueue.add(jsonObjRequest)
+    return true
+}
 
 fun syncOfflineMessageToDatabase(
     mContext: Context,
